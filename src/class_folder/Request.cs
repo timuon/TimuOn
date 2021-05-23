5979ac60a97bdac16c768e0704465526bfdfcf8e
Labros Androutsos <androutsos@ceid.upatras.gr>
2021-05-23 21:11:10 +0300

    Request class

---
+++ src/class_folder/Request.cs
@@ -0,0 +1,18 @@
+using System;
+using TempRescheduleRequestnamespace;
+
+namespace Requestnamespace
+{
+    public class Request : TempRescheduleRequest
+    {
+        public string Students { get; set; }
+        public string Reason {get; set;}
+        public string NewSchedule {get; set;}
+        
+        new public static void Main(string[] args)
+        {
+            Request request01= new Request();
+        }
+    }
+}
+
