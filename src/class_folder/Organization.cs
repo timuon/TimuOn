2b2f9cc05034c27833fec37ddd78cff4ee7e5468
Labros Androutsos <androutsos@ceid.upatras.gr>
2021-06-02 19:05:36 +0300

    Added method AddEvent to Organization class

--- src/class_folder/Organization.cs
+++ src/class_folder/Organization.cs
@@ -1,4 +1,5 @@
 using System;
+using Eventnamespace;
 using Usernamespace;
 
 namespace Organizationnamespace
@@ -9,8 +10,14 @@ namespace Organizationnamespace
         public string Administrators {get; set; }
         public string Auditoriums {get; set;}
         public bool Availability {get; set;}
-        public string Events {get; set;}
+        public string Events {get; zset;}
 
+        public static void addEvent(string name, DateTime Date1, string auditorium, int days_duration)
+        {
+            Event event02 = new Event(name, Date1,);
+
+            var event02 = new Event(Name = name, Date = Date1, Auditorium = auditorium, Duration = days_duration);
+        }
         new public static void Main(string[] args)
         {
             Organization organization01= new Organization();
