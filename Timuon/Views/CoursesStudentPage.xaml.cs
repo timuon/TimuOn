7912517a9822ab6e46f73a43a93372f85e8b1e66
Labros Androutsos <androutsos@ceid.upatras.gr>
2021-06-06 17:52:19 +0300

    Student Courses Page added

---
+++ Timuon/Views/CoursesStudentPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Timuon.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Shapes;
+
+namespace Timuon.Views
+{
+    public sealed partial class BlankPage : Page
+    {
+        public CoursesStudentViewModel ViewModel { get; } = new CoursesStudentViewModel();
+
+        public BlankPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlock_SelectionChanged(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
