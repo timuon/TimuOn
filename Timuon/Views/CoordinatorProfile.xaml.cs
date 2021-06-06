9764203aeb8f5166c2efdc0adba39b2d217685e2
Panagiotis Kavadias <p.kavvadias@upnet.gr>
2021-06-06 20:33:35 +0300

    Add personal details on profile page

--- Timuon/Views/CoordinatorProfile.xaml.cs
+++ Timuon/Views/CoordinatorProfile.xaml.cs
@@ -26,5 +26,10 @@ namespace Timuon.Views
         {
             this.InitializeComponent();
         }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
