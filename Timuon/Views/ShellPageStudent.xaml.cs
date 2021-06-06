a84ee7a7761c70975e0c5cdea8ff7a724c8959f5
Labros Androutsos <androutsos@ceid.upatras.gr>
2021-06-06 21:22:10 +0300

    Changes to Shell Page for Student

---
+++ Timuon/Views/ShellPageStudent.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Timuon.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace Timuon.Views
+{
+    // TODO WTS: Change the icons and titles for all NavigationViewItems in ShellPage.xaml.
+    public sealed partial class ShellPage : Page
+    {
+        public ShellStudentViewModel ViewModel { get; } = new ShellStudentViewModel();
+
+        public ShellPage()
+        {
+            InitializeComponent();
+            DataContext = ViewModel;
+            ViewModel.Initialize(shellFrame, navigationView, KeyboardAccelerators);
+        }
+    }
+}
