4aa0a141b1a62d760aa19094f4e73f34600a2a9b
Aristeidis Androutsopoulos <aristeidis97@gmail.com>
2021-06-06 23:39:10 +0300

    Several additions to Experience Tab

--- Timuon/Views/MainShellPage.xaml.cs
+++ Timuon/Views/MainShellPage.xaml.cs
@@ -9,7 +9,7 @@ namespace Timuon.Views
     // TODO WTS: Change the icons and titles for all NavigationViewItems in ShellPage.xaml.
     public sealed partial class ShellPage : Page
     {
-        public ShellViewModel ViewModel { get; } = new ShellViewModel();
+        public MainShellViewModel ViewModel { get; } = new MainShellViewModel();
 
         public ShellPage()
         {
