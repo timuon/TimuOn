805f27baf47b3ce3bedb2f72dfe9f57b68240727
Labros Androutsos <androutsos@ceid.upatras.gr>
2021-06-06 21:16:54 +0300

    View Model for Schedule Student added

---
+++ Timuon/ViewModels/ScheduleStudentViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace Timuon.ViewModels
+{
+    public class ScheduleStudentViewModel : ObservableObject
+    {
+        public ScheduleStudentViewModel()
+        {
+        }
+    }
+}
