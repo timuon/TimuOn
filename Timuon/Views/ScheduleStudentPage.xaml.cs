91bf652e6b616c91b17935268c1aa50b0d34466b
Labros Androutsos <androutsos@ceid.upatras.gr>
2021-06-10 21:47:42 +0300

    Added full visibility functionality for Schedule Page Student

--- Timuon/Views/ScheduleStudentPage.xaml.cs
+++ Timuon/Views/ScheduleStudentPage.xaml.cs
@@ -26,5 +26,79 @@ namespace Timuon.Views
         {
             this.InitializeComponent();
         }
+        private void Box_CheckedChanged(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if ((Yes1.IsChecked == true) & (No1.IsChecked == false))
+            {
+                EventCombo.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            }
+            else if ((No1.IsChecked == true) & (Yes1.IsChecked == false))
+            {
+                EventCombo.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            }
+        }
+        private void Event_SelectedIndexChanged(object sender, SelectionChangedEventArgs e)
+        {
+            HourCombo.Visibility = Windows.UI.Xaml.Visibility.Visible;
+
+        }
+
+        private void NewHour_SelectedIndexChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Appointment.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            AppointLine.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            Yes2.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            No2.Visibility = Windows.UI.Xaml.Visibility.Visible;
+
+        }
+        private void Box2_CheckedChanged(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if ((Yes2.IsChecked == true) & (No2.IsChecked == false))
+            {
+                ReasonTimePeriod.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                Userss.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                SubmitButton.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+
+            }
+            else if ((No2.IsChecked == true) & (Yes2.IsChecked == false))
+            {
+                
+                ReasonTimePeriod.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                SubmitButton.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                CommonHours.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                SelectHour1.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                Userss.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                SelectHour2.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                MessageMeeting.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+
+            }
+        }
+
+        private void Contanct_SelectedIndexChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CommonHours.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            SelectHour1.Visibility = Windows.UI.Xaml.Visibility.Visible;
+
+        }
+
+        private void NewPeriod_SelectedIndexChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (SelectHour1.SelectedValue.ToString() == "Other")
+            {
+                SelectHour2.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                SubmitButton.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                MessageMeeting.Visibility = Windows.UI.Xaml.Visibility.Visible;
+
+            }
+            else
+            {
+                SelectHour2.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                SubmitButton.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                MessageMeeting.Visibility = Windows.UI.Xaml.Visibility.Visible;
+
+
+
+            }
+        }
     }
 }
