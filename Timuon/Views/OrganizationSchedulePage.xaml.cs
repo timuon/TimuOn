d34d66084597278254329be7b563711b7c97a9eb
Labros Androutsos <androutsos@ceid.upatras.gr>
2021-06-10 03:01:45 +0300

    Organization schedule visibility for elements finished

--- Timuon/Views/OrganizationSchedulePage.xaml.cs
+++ Timuon/Views/OrganizationSchedulePage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Timuon.ViewModels;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -20,11 +21,32 @@ namespace Timuon.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+
     public sealed partial class OrganizationSchedulePage : Page
     {
+        public OrganizationScheduleViewModel ViewModel { get; } = new OrganizationScheduleViewModel();
+
         public OrganizationSchedulePage()
         {
             this.InitializeComponent();
+        }
+        private void Actionchanged_SelectedIndexChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ActionsCombobox.SelectedValue.ToString() == "Change the availability of an auditorium")
+            {
+                ChangeAvail.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                Audit.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            }
+            else
+            {
+                ChangeAvail.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                Audit.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            }
+            WhichDate.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            CalendarDate.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            ForHowLong.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            Reason.Visibility = Windows.UI.Xaml.Visibility.Visible;
+
         }
     }
 }
