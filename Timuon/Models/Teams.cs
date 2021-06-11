13273732b1929b6dc05771e5dd66b323169200fa
Aristeidis Androutsopoulos <aristeidis97@gmail.com>
2021-06-11 17:02:03 +0300

    History and classes for teams and course changes

--- Timuon/Models/Teams.cs
+++ Timuon/Models/Teams.cs
@@ -8,15 +8,23 @@ namespace Timuon.Models
 {
     public class Teams
     {
+        public string Name { get; set; }
         public List<string> Students_teams { get; set; }
+        public int num_of { get; set; }
 
-        public Teams(List<string> students)
+
+        public Teams(List<string> students, int num, string name)
         {
             Students_teams = students;
-        }
+            num_of = num;
+            Name = name;
+            
+    }
 
-        public void get_Team_Info()
+        public string get_Team_Info(List<string> myList)
         {
+            string combindedString = string.Join(",", myList);
+            return combindedString;
             //returns info about teams. C# UWP does this in the back-code of View object. For simplicity it is left there. Can be moved here for more complicated hierarchy
         }
 
